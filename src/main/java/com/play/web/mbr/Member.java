ef875a6cec85f21971a2748c1e7edc3ee791b233
kingofahn <kingofahn@gmail.com>
2018-10-15 09:56:57 +0900

    first commit

---
+++ src/main/java/com/play/web/mbr/Member.java
@@ -0,0 +1,12 @@
+package com.play.web.mbr;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data @Lazy
+public class Member {
+	private String userid, password, name, ssn, age, gender, teamid, roll, subject, email, phone;
+}
